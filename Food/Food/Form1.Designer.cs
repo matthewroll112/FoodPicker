528203d342b71848a5bf0d34a832d4c8c71d2b34
matthewroll112 <matthewroll112@gmail.com>
2022-06-10 19:02:38 +1200

    Finish off

---
+++ Food/Food/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Food
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.buttonRoll = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxAdd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonListClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 31);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(194, 379);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // buttonRoll
+            // 
+            this.buttonRoll.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.buttonRoll.Location = new System.Drawing.Point(232, 353);
+            this.buttonRoll.Name = "buttonRoll";
+            this.buttonRoll.Size = new System.Drawing.Size(191, 57);
+            this.buttonRoll.TabIndex = 1;
+            this.buttonRoll.Text = "Choose";
+            this.buttonRoll.UseVisualStyleBackColor = false;
+            this.buttonRoll.Click += new System.EventHandler(this.buttonRoll_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.Color.RosyBrown;
+            this.buttonClear.Location = new System.Drawing.Point(429, 353);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(191, 57);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBoxAdd
+            // 
+            this.textBoxAdd.Location = new System.Drawing.Point(332, 49);
+            this.textBoxAdd.Name = "textBoxAdd";
+            this.textBoxAdd.Size = new System.Drawing.Size(190, 20);
+            this.textBoxAdd.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Add options";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(332, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(332, 173);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 117);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonListClear
+            // 
+            this.buttonListClear.BackColor = System.Drawing.Color.Silver;
+            this.buttonListClear.Location = new System.Drawing.Point(36, 416);
+            this.buttonListClear.Name = "buttonListClear";
+            this.buttonListClear.Size = new System.Drawing.Size(139, 31);
+            this.buttonListClear.TabIndex = 12;
+            this.buttonListClear.Text = "Delete";
+            this.buttonListClear.UseVisualStyleBackColor = false;
+            this.buttonListClear.Click += new System.EventHandler(this.buttonListClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonListClear);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxAdd);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonRoll);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Food Decider";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button buttonRoll;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonListClear;
+    }
+}
+
---
+++ Food/Food/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Food
+{
+    public partial class Form1 : Form
+    {
+        const string fileName = "places.txt";
+        string[] all;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string line;
+            StreamReader read;
+            read = File.OpenText(fileName);
+            while (!read.EndOfStream)
+            {
+                //Read Line
+                line = read.ReadLine();
+                //Split at comma
+                all = line.Split(',');
+                foreach(string name in all)
+                {
+                    checkedListBox1.Items.Add(name);
+                }
+            }
+            read.Close();
+            
+        }
+
+        private void buttonRoll_Click(object sender, EventArgs e)
+        {
+            //Clearing pb
+            pictureBox1.Refresh();
+
+            //Rng
+            var random = new Random();
+
+            //Index
+            int index = random.Next(0, checkedListBox1.CheckedItems.Count);
+
+            //Creating graphics
+            Graphics graphics = pictureBox1.CreateGraphics();
+
+            //Displaying option
+            using (Font myFont = new Font("Arial", 14))
+            {
+                graphics.DrawString(checkedListBox1.CheckedItems[index].ToString(), myFont, Brushes.Black, new Point(2, 2));
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //Adding items sp[ecified by text box to list
+            checkedListBox1.Items.Add(textBoxAdd.Text);
+
+            StreamWriter write;
+            write = File.AppendText(fileName);
+            write.Write(","+textBoxAdd.Text);
+            write.Close();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxAdd.Clear();
+            pictureBox1.Refresh();
+        }
+
+        private void buttonListClear_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            foreach(string s in checkedListBox1.CheckedItems)
+            {
+                all.Where(x => !s.Equals(x)).ToArray();
+            }
+            StreamWriter write;
+            File.WriteAllText(fileName, string.Empty);
+            write = File.AppendText(fileName);
+            write.Write(all[0]);
+            for(int i = 1; i < all.Length; i++)
+            {
+                write = File.AppendText(","+all[i]);
+            }
+            write.Close(); 
+        }
+    }
+}
